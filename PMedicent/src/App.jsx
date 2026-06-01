3a432b34c6ff85cc8ef2cc0d59464daf68252087
Ricy24 <discordandres20@gmail.com>
2026-06-01 15:43:46 -0500

    subiendo plantilla

---
+++ PMedicent/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Inicio from './pages/Inicio'
+
+function App() {
+  return (
+    <div className="App">
+      <Inicio />
+    </div>
+  )
+ 
+}
+
+export default App
---
+++ PMedicent/src/pages/Inicio.jsx
@@ -0,0 +1,9 @@
+function Inicio() {
+  return (
+    <div>
+      <h1>Este es mi inicio</h1>
+     
+    </div>
+  );
+}
+export default Inicio;
